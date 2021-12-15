94d07fd1910c322e07abc412f70a148d29371f32
skaiste pakalniskyte <skaiste.sky@gmail.com>
2021-12-15 12:36:48 +0000

    second commit

---
+++ game.js
@@ -0,0 +1,77 @@
+const textElement = document.getElementById('text')
+const optionButtonsElement = document.getElementById('option-buttons')
+
+let state = {}
+
+function startGame() {
+state = {}
+showTextNode(1)
+}
+
+function showTextNode(textNodeIndex) {
+const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+textElement.innerText = textNode.text
+while (optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+}
+
+textNode.options.forEach(option => {
+    if (showOption(option)) {
+        const button = document.createElement('button')
+        button.innerText = option.text
+        button.classList.add('btn')
+        button.addEventListener('click', () => selectOption(option))
+        optionButtonsElement.appendChild(button)
+    }
+})
+}
+
+function showOption(option) {
+    return true
+}
+
+function selectOption(option) {
+
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'You wake up in a strange place and you see a jar of blue goo near you',
+        options: [
+            {
+                text: 'Take goo',
+                setState: { blueGoo: true },
+                nextText: 2
+            },
+            {
+                text: 'Leave the good',
+                nextText: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+        options: [
+            {
+                text: 'Trade the goo for a sword',
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, sword:true },
+                nextText: 3
+            },
+            {
+                text: 'Trade the goo for a shield',
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: { blueGoo: false, shield:true },
+                nextText: 3
+            },
+            {
+                text: 'Ignore the merchant',
+                nextText: 3
+            },
+        ]
+    }
+]
+
+startGame()
