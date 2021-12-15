1f92d531a7bc54db96aad5270e8c8efb09969085
skaiste pakalniskyte <skaiste.sky@gmail.com>
2021-12-15 17:10:53 +0000

    fourth commit

--- game.js
+++ game.js
@@ -1,13 +1,24 @@
+
+// This is selecting the text element and targeting it by id set in html
+// This is selecting the buttons element and targeting it by id set in html (called it optionButtonsElement because this is an actual element in the application)
 const textElement = document.getElementById('text')
 const optionButtonsElement = document.getElementById('option-buttons')
 
+// State variable that's equal to an empty object, allows to keep a track of what the character has on them for example the goo
 let state = {}
 
+// This is a function to start up the game and set all of the state and application to where it needs to be
+// When we start our game we want to take our state and make sure that this is an empty object
+// Also we want to show the next text node, so we use showTextNode and type (1) to show the very first one
 function startGame() {
 state = {}
 showTextNode(1)
 }
 
+// This is a function that allows to display whichever option we're on
+//To implement the show text node just going to first get the text node which is going to be equal to textNodes.find and this is going to take in a text node for each one in the array and we want
+//to find the one that has current ID so we will say textNode.id is going to be equal to textNodeIndex. 
+//
 function showTextNode(textNodeIndex) {
 const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
 textElement.innerText = textNode.text
@@ -30,6 +41,8 @@ function showOption(option) {
     return option.requiredState == null || option.requiredState(state)
 }
 
+// This is a function that is going to happen every time an option is selected
+// SelectOption(option) is going to take whichever option that we select because we need to know which option we're selecting 
 function selectOption(option) {
 const nextTextNodeId = option.nextText
 if (nextTextNodeId <= 0) {
@@ -39,6 +52,9 @@ state = Object.assign(state, option.setState)
 showTextNode(nextTextNodeId)
 }
 
+// Variable to define textNodes
+//Inside is an object that is going to have an id of 1 (for first text node), text, options
+//Options within the object will show us what we can do. It will have parameters for text (that will show up on the button), setState (for example true), nextText (target id for example '2') as this is what we want to see next
 const textNodes = [
     {
         id: 1,
@@ -192,4 +208,5 @@ const textNodes = [
         }
 ]
 
+// This allows the game to start as soon as the page loads
 startGame()
